e07efed7ee24fac46a06a1a94e4850d7e575aaf0
Akhmad Bakir <akhmgazbak@gmail.com>
2023-11-20 00:51:28 +0300

    Labs 1 Control work

---
+++ TriangleArea/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длину стороны равностороннего треугольника");
+            double sideLength = Convert.ToInt32(Console.ReadLine());
+            double halfTrianglePerimeter = 3 * sideLength / 2;
+            double triangleArea = Math.Sqrt(halfTrianglePerimeter * (Math.Pow ((halfTrianglePerimeter - sideLength), 3)));
+            Console.WriteLine("Площадь равностороннего треугольника со стороной {0} равна {1}", sideLength, triangleArea.ToString("0.00"));
+        
+        }
+    }
+}
