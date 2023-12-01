2fea400ca3b775574786e017498a3e0d162d637d
Akhmad Bakir <akhmgazbak@gmail.com>
2023-12-01 08:45:01 +0300

    Labs04 and Labs05

---
+++ Lab04/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp2
+{    
+    public class Program
+    {
+        static void Func(double a, double b, double c, out double x1, out double x2, out int flag)
+        {
+            double d = b * b - 4 * a * c;
+            x1 = (-b + Math.Sqrt(d)) / (2 * a);
+            x2 = (-b - Math.Sqrt(d)) / (2 * a);
+            if (d > 0)
+            {
+                flag = 1;
+            }
+            else if (d == 0)
+            {
+                flag = 0;
+            }
+            else
+            {
+                flag = -1;
+            }
+        }
+        public static void Main()
+        {
+            Console.WriteLine("Введите значение коэффиента a:");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите значение коэффиента b:");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите значение коэффиента c:");
+            double c = Convert.ToDouble(Console.ReadLine());
+            double x1;
+            double x2;
+            int flag;
+            Func(a, b, c, out x1, out x2, out flag);
+            if (flag > 0) Console.WriteLine($"Корни уравнения с коэффициентами a = {a}, b = {b}, c = {c} равны: x1 = {x1}, x2 = {x2}");
+            else if (flag == 0) Console.Write($"Корень уравнения с коэффициентами a = {a}, b = {b}, c = {c} один: x1= x2 = {x1}");
+            else Console.WriteLine($"Корней уравнения с коэффициентами a = {a}, b = {b}, c = {c} нет.");
+        }
+
+    }
+
+
+}
---
+++ Lab05/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+class Lab06
+{
+        static void Main()
+    {
+        int[] a = new int[5];
+        Input(a);
+        int sum = Sum(a);
+        Console.WriteLine(sum);
+        int ave = Average(a);
+        Console.WriteLine(ave);
+        int sumMinus = SumMinus(a);
+        Console.WriteLine(sumMinus);
+        int sumPlus = SumPlus(a);
+        Console.WriteLine(sumPlus);
+    }
+
+    static void Input(int[] a)
+    {
+        for (int i = 0; i < a.GetLength(0); i++)
+        {
+            Console.Write("Enter value: ", i);
+            a[i] = int.Parse(Console.ReadLine());
+        }
+    }
+    static int Sum(int[] a)
+    {
+        int result = a[0] + a[1] + a[2] + a[3] + a[4];
+        return result;
+    }
+    static int Average(int[] a)
+    {
+        int result = (a[0] + a[1] + a[2] + a[3] + a[4]) / a.Length;
+        return result;
+    }
+    static int SumMinus(int[] a)
+    {
+        int sumMinus=0;
+        for (int i = 0; i < a.Length; i++)
+        {
+            if (a[i] < 0)
+                sumMinus += a[i];
+        }
+        return sumMinus;                              
+    }
+    static int SumPlus(int[] a)
+    {
+        int sumPlus = 0;
+        for (int i = 0; i < a.Length; i++)
+        {
+            if (a[i] < 0)
+                sumPlus += a[i];
+        }
+        return sumPlus;
+    }
+}
+    
